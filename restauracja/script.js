2c265869c28fde5b814894934b8b4ced4a74388c
kooowal <93763074+kooowal@users.noreply.github.com>
2022-05-12 01:24:33 +0200

    Add files via upload

---
+++ restauracja/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#logo1").click(function(){
+        $("#logo1").animate({
+            opacity: '50%',
+            width: '70%'
+        });
+        $("#logo1").animate({
+            opacity: '100%',
+            width: '100%'
+        });
+        $("#logo1").animate({
+            opacity: '50%',
+            width: '70%'
+        });
+        $("#logo1").animate({
+            opacity: '100%',
+            width: '100%'
+        });
+        $("#logo1").animate({
+            opacity: '50%',
+            width: '40%'
+        });
+        $("#logo1").animate({
+            opacity: '100%',
+            width: '100%'
+        });
+    });
+});
+  
